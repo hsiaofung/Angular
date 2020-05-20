cdc71cc0a064ee2015ae0b1114e6bff611171332
hsiaofung <hsiaofung.chou@gmail.com>
2020-05-20 21:59:01 +0800

    add 元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { Stock } from './model/stock';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,18 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./app.component.css'],
   encapsulation: ViewEncapsulation.None, // 使用全域CSS
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'stock-market';
+  public stockObj: Stock;
+  public stock: Stock;
+
+  ngOnInit(): void {
+    this.stockObj = new Stock('Test Stock Company', 'TSC', 85, 80);
+    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+  }
+
+  onToggleFavorite(stock: Stock) {
+    console.log('Favorite for stock ', stock, ' was triggered');
+    this.stock.favorite = !this.stock.favorite;
+  }
 }
--- src/app/stock/stock-item-1/stock-item.component.ts
+++ src/app/stock/stock-item-1/stock-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Stock } from '../../model/stock';
 
 @Component({
@@ -14,8 +14,14 @@ export class StockItemComponent implements OnInit {
   // public positiveChange: boolean; // 加入公開變數
   // public favorite: boolean; // 加入公開變數
 
+  //對成員變數加上Input，以傳入stock 物件，而非從元件中初始化。
+  @Input() public stock: Stock;
+
+  //
+  @Output() private toggleFavorite: EventEmitter<Stock>;
+
   // 宣告一個變數stock 為Stock型別
-  public stock: Stock;
+  //public stock: Stock;
 
   // 宣告一個變數stocks作為股票清單，而非個別股票
   public stocks: Array<Stock>;
@@ -24,7 +30,12 @@ export class StockItemComponent implements OnInit {
   public stockClasses;
   public stockStyles;
 
-  constructor() {}
+  constructor() {
+    this.toggleFavorite = new EventEmitter<Stock>();
+  }
+  onToggleFavorite(event) {
+    this.toggleFavorite.emit(this.stock);
+  }
   // 元件初始值
   ngOnInit() {
     // 基本的寫法 :
@@ -36,7 +47,7 @@ export class StockItemComponent implements OnInit {
     // this.favorite = false;
 
     // 使用TypeScript 定義型別的寫法:
-    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+    //this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
 
     // 初始化stocks
     this.stocks = [
@@ -64,16 +75,16 @@ export class StockItemComponent implements OnInit {
   }
 
   // 點擊時觸發的函式
-  toggleFavorite(event, index) {
-    // 使用event存取底層DOM事件
-    console.log(
-      'We are toggling the favorite state for this stock',
-      index,
-      event
-    );
-    // this.favorite = !this.favorite;
-    this.stocks[index].favorite = !this.stocks[index].favorite;
-  }
+  // toggleFavorite(event, index) {
+  //   // 使用event存取底層DOM事件
+  //   console.log(
+  //     'We are toggling the favorite state for this stock',
+  //     index,
+  //     event
+  //   );
+  //   // this.favorite = !this.favorite;
+  //   this.stocks[index].favorite = !this.stocks[index].favorite;
+  // }
 
   // 使用函數回傳值來決定如何識別個別元素。
   trackStockByCode(index, stock) {
