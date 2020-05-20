3cce7c553018a25e33d126868563f83a412edd90
tim <tim_chou@chowsangsang.com>
2020-05-20 17:43:49 +0800

    add *ngFor in product

--- src/app/product/product-item-2/product-item.component.ts
+++ src/app/product/product-item-2/product-item.component.ts
@@ -11,6 +11,9 @@ export class ProductItemComponent implements OnInit {
   public cartCount: number;
   public removeBtnDisable: boolean;
 
+  // 宣告變數products為商品清單
+  public products: Array<Product>;
+  public quantities: Array<number>;
   constructor() {}
 
   ngOnInit() {
@@ -22,6 +25,15 @@ export class ProductItemComponent implements OnInit {
     );
     this.cartCount = 0;
     this.removeBtnDisable = false;
+    this.products = [
+      new Product('戒指', 2000, '../../assets/nav-thm.jpg', false),
+      new Product('項鍊', 3000, '../../assets/nav-thm.jpg', false),
+      new Product('手鐲', 4000, '../../assets/nav-thm.jpg', false),
+    ];
+    this.quantities = [];
+    for (let i = 0; i <= 20; i++) {
+      this.quantities.push(i);
+    }
   }
   addToCart() {
     this.cartCount += 1;
