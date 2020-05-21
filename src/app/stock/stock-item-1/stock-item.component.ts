b9dcd95db623baf90de2468a4a11965e86ded94d
hsiaofung <hsiaofung.chou@gmail.com>
2020-05-21 19:23:58 +0800

    add test

--- src/app/stock/stock-item-1/stock-item.component.ts
+++ src/app/stock/stock-item-1/stock-item.component.ts
@@ -87,22 +87,22 @@ export class StockItemComponent
       new Stock('Last Stock Company', 'LSC', 876, 765),
     ];
 
-    const diff = this.stock.price / this.stock.previousPrice - 1;
-    const largeChange = Math.abs(diff) > 0.01;
+    // const diff = this.stock.price / this.stock.previousPrice - 1;
+    // const largeChange = Math.abs(diff) > 0.01;
 
     // ngClass的寫法
-    this.stockClasses = {
-      positive: this.stock.isPositiveChange(),
-      negative: !this.stock.isPositiveChange(),
-      'large-change': largeChange,
-      'small-change': !largeChange,
-    };
+    // this.stockClasses = {
+    //   positive: this.stock.isPositiveChange(),
+    //   negative: !this.stock.isPositiveChange(),
+    //   'large-change': largeChange,
+    //   'small-change': !largeChange,
+    // };
 
     // ngStyle的寫法
-    this.stockStyles = {
-      color: this.stock.isPositiveChange() ? 'green' : 'red',
-      'font-size': largeChange ? '1.2em' : '0.8em',
-    };
+    // this.stockStyles = {
+    //   color: this.stock.isPositiveChange() ? 'green' : 'red',
+    //   'font-size': largeChange ? '1.2em' : '0.8em',
+    // };
     console.log('Stock Item Component - On Init');
   }
 
